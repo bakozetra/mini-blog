24c54a55c6a6e5b8a418172786fc61a5b06a3c75
Synthia Hanitriniaina <synthia.han@onja.org>
2020-07-04 15:11:53 +0300

    add change in the function and in the event listener

--- script.js
+++ script.js
@@ -1,56 +1,28 @@
-console.log('it works');
- /*
-//code your solution in here
-// function 
-function post(title,content) {
-        this.ttle = title;
-        this.content= content;
-    }
-
-
-// Blog  object code 
- const post = () => {
-  this.post = [];
-
- }
-  // this is the new post
-
+// function
 const newPost = () => {
-    const div = document.getElementById("post-list");
-    const newParagraph = document.createElement("p");
-    const newHeading = document.createElement("h5");
-    const newTag = document.createElement("span");
-    var blog_title = blog.post.newHeading;
-    var blog_paragraph = blog.post.newParagraph;
-    var blog_tag = blog.post.newTag;
-    newHeading.textContent = blog_title;
-    newParagraph.textContent = blog_paragraph;
-    newTag.textContent = blog_tag;
-    newPost.appendChild(newHeading);
-    newPost.appendChild(newParagraph);
-    newPost.appendChild(newTag);
-    return div;
-};
- // the new blog
- const blog = () => {
+    const div = document.createElement("div");
+    div.setAttribute("id", "post-list");
 
- }
-*/
-const newPost = () => {
-    const div = document.getElementById("post-list");
     const title = document.createElement("h5");
-    const content = document.createElement("p");
-     title.textContent = "second blog";
-     content.textContent = " this is the good to know";
-     div.appendChild(title);
-     div.appendChild(content);
-      return div;
-}
+    title.classList.add("card-title");
 
-var submit = document.getElementById('submit');
- // event listener
-  submit.addEventListener("click", ($event) => {
-  var jsTitle = document.getElementById("title_input").value;
-  var jsContent = document.getElementById("content_input").value;
-  var firstPost = newPost();
-  })
+    const content = document.createElement("p");
+    content.classList.add("card-text");
+
+    title.textContent = document.getElementById("new-post-title").value;
+    content.textContent = document.getElementById("new-post-content").value;
+    div.appendChild(title);
+    div.appendChild(content);
+    return div;
+}
+// submit
+
+var submit = document.getElementById('submit-form');
+const newBlog = document.getElementById("post-list");
+// event listener
+submit.addEventListener("click", ($event) => {
+    var addNewPost = newPost();
+    newBlog.appendChild(addNewPost);
+    $event.preventDefault();
+    document.getElementById("form").reset();
+});
